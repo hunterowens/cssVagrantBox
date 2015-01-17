7f88da878a4c233a5b423ee6c6f863582624021c
hunterowens <owens.hunter@gmail.com>
2015-01-17 14:29:25 -0600

    mysql client

--- fabfile.py
+++ fabfile.py
@@ -43,7 +43,8 @@ REQUIRED_DEBIAN_PACKAGES = [
     'git',
     'libsm6',
     'libxrender-dev',
-    'wget']
+    'wget',
+    'mysql-client']
 
 SOFTWARES_URLS = {
         'anaconda': 'http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-Linux-x86_64.sh'}  # noqa
